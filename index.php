e3821431d480b3118f21a2809ac0a427560ceffe
Marcel Liebgott <marcel.liebgott@googlemail.com>
2016-06-23 22:08:00 +0200

    hint possible php error

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ require_once CONFIG_DIR . SQL_FILE;
 require_once 'libs/autoload.php';
 
 if(DEBUG_MODE){
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
+	@ini_set('display_errors', 1);
+	@ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 }
 
