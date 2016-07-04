ac833dd44179b7b11d3450cdae6fba7f8a0598f1
Marcel Liebgott <marcel.liebgott@materna.de>
2016-07-04 21:00:46 +0200

    merging

--- libs/mvc/view.php
+++ libs/mvc/view.php
@@ -7,9 +7,13 @@ if(!defined('PATH')){
  * Description of View
  *
  * @author Marcel Liebgott <Marcel@mliebgott.de>
+ * @package libs.mvc
  * @since 1.00
  */
-class FW_Mvc_View extends FW_Mvc_ViewParser{
+class FW_Mvc_View extends FW_Object{
+	private $useHeadline = true;
+	private $useFooter = true;
+	
     private $template;
     private $class;
     
@@ -24,6 +28,19 @@ class FW_Mvc_View extends FW_Mvc_ViewParser{
     private $include_regex = '#\{include\s*(.+)\s*\"(.+)\"\}#s';
     //private $include_regex = '#\{include\s*(.+)\s*\"(.+)\"\s*(?:\s*(.+?))?\}#s';
     private $bbcode_regex = '#\{bbcode\}\s*(.+?)\s*\{endbbcode\}#s';
+
+    private $enableKindsOfIncludes = array('template' => 'views/');
+    private $enableKindsOfElements = array('session:' => 'FW_Session', 'cookie:' => 'FW_Cookie');
+    private $enableOperations = array('==', '>', '<', '>=', '<=', '!=');
+    private $enabledConst = array('true' => true, 'false' => false);
+
+    protected $msg;
+    protected $lang;
+    protected $vars = array();
+
+    const BEGIN_DELIMITER = '{';
+    const END_DELIMITER = '}';
+    const VARIABLE_IDENT = '$';
     
     /**
      * constructor
@@ -32,9 +49,61 @@ class FW_Mvc_View extends FW_Mvc_ViewParser{
      * @param FW_Abstract_Controller $class
      */
     public function __construct($class){
+		parent::__construct();
+		
         $this->class = $class;
-        parent::__construct();
+
+        $this->lang = FW_Registry::getInstance()->getLanguage();
     }
+	
+	/**
+	 * set the headline property of this page
+	 *
+	 * @access public
+	 * @param boolean $headline
+	 * @throws FW_Exception_WrongParameter
+	 */
+	public function setUseHeadline($headline){
+		if(!FW_Validate::isBoolean($headline)){
+			throw new FW_Exception_WrongParameter("use header must be an boolean");
+		}
+		
+		$this->useHeadline = $headline;
+	}
+	
+	/**
+	 * if the page needed a headline
+	 *
+	 * @access public
+	 * @return boolean
+	 */
+	public function useHeadline(){
+		return $this->useHeadline;
+	}
+	/**
+	 * set the footer property of this page
+	 *
+	 * @access public
+	 * @param boolean $footer
+	 * @throws FW_Exception_WrongParameter
+	 */
+	public function setUseFooter($footer){
+		if(!FW_Validate::isBoolean($footer)){
+			throw new FW_Exception_WrongParameter("use header must be an boolean");
+		}
+		
+		$this->useFooter = $footer;
+	}
+	
+	/**
+	 * if the page needed a footer
+	 *
+	 * @access public
+	 * @return boolean
+	 */
+	public function useFooter(){
+		return $this->useFooter;
+	}
     
     /**
      * render
@@ -61,18 +130,21 @@ class FW_Mvc_View extends FW_Mvc_ViewParser{
             throw new FW_Exception("template file dosn't exists");
         }
         
-        $this->headerFile =  $dir . 'inc/header.php';
-        $this->footerFile = $dir . 'inc/footer.php';
-        $this->msgFile = $dir . 'inc/msg.php';
-        
+		$this->msgFile = $dir . 'inc/msg.php';
+		
+		$this->headerFile =  $dir . FW_TPL_HEADER;
         $this->template = file_get_contents($this->headerFile);
 
-        if(in_array('headline', $this->vars) && $this->vars['headline'] === true){
-            $this->template .= file_get_contents($dir . 'inc/headline.php');
+        if($this->useHeadline()){
+            $this->template .= file_get_contents($dir . FW_TPL_HEADLINE);
         }
 
         $this->template .= file_get_contents($this->templateFile);
-        $this->template .= file_get_contents($this->footerFile);
+		
+		if($this->useFooter()){
+			$this->footerFile = $dir . 'inc/footer.php';
+			$this->template .= file_get_contents($this->footerFile);
+		}
         
         $this->renderInformation();
         $this->renderStatements();
@@ -294,6 +366,153 @@ class FW_Mvc_View extends FW_Mvc_ViewParser{
      */
     private function parseLang($result){
         return $this->replace($result[0], $this->lang->getLangValue($result[1]), $result[0]);
+    }
+
+    /**
+     * replace a string in a string
+     * 
+     * @access private
+     * @param string $search
+     * @param string $replace
+     * @param string $subject
+     * @return string
+     */
+    protected function replace($search, $replace, $subject){
+        return str_replace($search, $replace, $subject);
+    }
+    
+    /**
+     * parse an include statement
+     * 
+     * @access protected
+     * @param array $result
+     * @return mixed
+     */
+    protected function includeParse($result){
+        $type = str_replace(' ', '', $result[1]);
+        if(!in_array($type, array_keys($this->enableKindsOfIncludes))){
+            $this->msg->setMessage('not supported kind of include', FW_Messages::_E_ERROR);
+            return null;
+        }
+        
+        // exists file
+        $templateFile = $this->enableKindsOfIncludes[$type] . $result[2] . '.php';
+        
+        if(!file_exists($templateFile)){
+            $this->msg->setMessage("can't find template file<br>" . $templateFile, FW_Messages::_E_ERROR);
+            return null;
+        }
+
+        $content = file_get_contents($templateFile);
+
+        if(isset($result[3])){
+            foreach($this->vars[$result[3]] as $key => $value){
+                $content = $this->replace(self::BEGIN_DELIMITER . self::VARIABLE_IDENT . $key . self::END_DELIMITER, $value, $content);
+            }
+        }
+        
+        return $content;
+    }
+    
+    /**
+     * replace an language ident
+     * 
+     * @access protected
+     * @param string $found
+     * @return string
+     */
+    protected function languageParse($found){
+        return $this->lang->getLangValue($found);
+    }
+    
+    /**
+     * add variables who replace to
+     * 
+     * @access public
+     * @param array $vars
+     */
+    public function addVariables(array $vars){
+        $keys = array_keys($vars);
+        
+        if(array_search('headline', $keys) === false){
+            $this->vars = array_merge($this->vars, array('headline' => true));
+        }
+        
+        if(array_search('footer', $keys) === false){
+            $this->vars = array_merge($this->vars, array('footer' => true));
+        }
+        
+        $tmp_var = array_merge($this->vars, $vars);
+        $this->vars = $tmp_var;
+    }
+    
+    /**
+     * parse an if statement and compare the condition
+     * 
+     * @access protected
+     * @throws FW_Exception
+     * @param array $result
+     * @return string
+     */
+    protected function parseIfElse($result){
+        $condition = false;
+        $con = null;
+        $if_else_match = array();
+        preg_match_all('/(.*):(.*)\s+(.*)\s+(.*)/i', $result[1], $if_else_match);
+        
+        if(count($if_else_match[0]) > 0){
+            $search = $if_else_match[1][0] . ':';
+            if(in_array($search, array_keys($this->enableKindsOfElements))){                
+                $class = $this->enableKindsOfElements[$search];
+                    
+                $value = $class::get($if_else_match[2][0]);
+                
+                $con = str_replace($search . $if_else_match[2][0], "'$value'", $if_else_match[0][0]);
+            }else{
+                throw new FW_Exception("not supported template instance: " . $if_else_match[1][0]);
+            }
+        }
+        
+        $var_match = array(); 
+        preg_match_all('/\$(.*)\s+(.*)\s+(.*)/i', $result[1], $var_match);
+        
+        if(count($var_match[0]) > 0){
+            $var_arr = explode('.', $var_match[1][0]);
+
+            // array element wanted
+            if(count($var_arr) > 0){
+                if(in_array($var_arr[0], array_keys($this->vars))){
+                    $val = $this->vars[$var_arr[0]];
+                    $value = null;
+                    
+                    if(is_array($val)){
+                        if(in_array($var_arr[1], array_keys($val))){
+                            $value = $val[$var_arr[1]];
+                        }else{
+                            throw new FW_Exception("variable (" . $var_arr[1] . ") not in array of " . $var_arr[0]);
+                        }
+                    }else{
+                        $value = $val;
+                    }
+                    
+                    $con = str_replace("$" . $var_match[1][0], "'$value'", $var_match[0][0]);
+                }
+            }
+        }
+        
+        if(!empty($con)){
+            eval('$condition = ' . $con . ';');
+            
+            if($condition){
+                return $result[2];
+            }else{
+                if(isset($result[3])){
+                    return $result[3];
+                }else{
+                    return '';
+                }
+            }
+        }
     }
 }
 ?>
