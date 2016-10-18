3a0149992b3390826cb58abf3f5cbbf22e719142
Marcel Liebgott <marcel.liebgott@googlemail.com>
2016-10-18 20:51:38 +0200

    update string unit test

--- tests/StringTest.php
+++ tests/StringTest.php
@@ -1,50 +1,68 @@
 <?php
-
-define('PATH', '');
+if(!defined('PATH')){
+	define('PATH', '');
+}
 
 include_once 'libs/string.php';
 
 class StringTest extends PHPUnit_Framework_TestCase{
 	private $string = "MVC-Framework";
+	private $mailMatcher;
 	
 	public function test_substr(){
 		$result = "Framework";
 		
 		$substr = FW_String::substr($this->string, 4);
 		
-		$this->assertEquals($substr, $result);
+		$this->assertEquals($result,$substr);
 	}
 	
 	public function test_substrLenght(){
-		$result = "MVC";
+		$expected = "MVC";
 		
 		$substr = FW_String::substr($this->string, 0, 3);
 		
-		$this->assertEquals($substr, $result);
+		$this->assertEquals($substr, $expected);
 	}
 	
 	public function test_strlen(){
-		$stringLenght = 13;
+		$expected = 13;
 		
 		$lenght = FW_String::strlen($this->string);
 		
-		$this->assertEquals($lenght, $stringLenght);
+		$this->assertEquals($lenght, $expected);
 	}
 	
 	public function test_strtolower(){
-		$lowercase = "mvc-framework";
+		$expected = "mvc-framework";
 		
 		$result = FW_String::strtolower($this->string);
 		
-		$this->assertEquals($lowercase, $result);
+		$this->assertEquals($expected, $result);
 	}
 	
 	public function test_strtoupper(){
-		$uppercase = "MVC-FRAMEWORK";
+		$expected = "MVC-FRAMEWORK";
 		
 		$result = FW_String::strtoupper($this->string);
 		
-		$this->assertEquals($uppercase, $result);
+		$this->assertEquals($expected, $result);
+	}
+	
+	public function test_strpos(){
+		$expected = 3;
+		
+		$result = FW_String::strpos($this->string, '-');
+		
+		$this->assertEquals($result, $expected);
+	}
+	
+	public function test_substrCount(){
+		$expected = 1;
+		
+		$result = FW_String::substrCount($this->string, '-');
+		
+		$this->assertEquals($expected, $result);
 	}
 }
 ?>
